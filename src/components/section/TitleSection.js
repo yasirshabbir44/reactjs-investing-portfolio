455d3e1290a7c7a4a991f0bae1d49131c32afbe7
Yasir Shabbir <yasirshabbir44@gmail.com>
2024-01-22 21:03:44 +0400

    add Title section

---
+++ src/components/section/TitleSection.js
@@ -0,0 +1,24 @@
+// ProfileSection.js
+import React from 'react';
+import CustomCard from '../CustomCard';
+
+const TitleSection = () => {
+
+
+    return (
+        <CustomCard title="Apple Inc (AAPL)">
+            <p>NASDAQ: AAPL</p>
+            <p>Currency in USD</p>
+            <p>
+                <strong>Live Price:</strong> $195.01
+            </p>
+            <p>
+                <strong>Exchange:</strong> NASDAQ
+            </p>
+            <p>Disclaimer: This is just a sample app. Data may not be accurate.</p>
+            {/* Add more details as needed */}
+        </CustomCard>
+    );
+};
+
+export default TitleSection;
